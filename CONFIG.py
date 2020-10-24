47cdd5876d09ae99bd19674b95de76469212fe90
unknown <rajatkulkarni48@gmail.com>
2020-10-24 13:20:09 +0530

    first commit

---
+++ CONFIG.py
@@ -0,0 +1,10 @@
+erase_hmax=122
+erase_hmin=96
+erase_satmax=255
+erase_satmin=38
+erase_valmax=255
+erase_valmin=219
+
+mycolours=[[0,148,199,17,255,255]] #you can add more colours by passing the hmin,satmin,valmin,hmax,,satmax,valmax
+
+mycolourvalues=[[0,128,255]] #add more colours to draw for different colours of object detected
